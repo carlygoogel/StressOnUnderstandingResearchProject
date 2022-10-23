ba8c3db143e25ad5d9f4f85eccc51cff7936045a
carlygoogel <77814476+carlygoogel@users.noreply.github.com>
2022-10-23 11:12:26 -0400

    Add files via upload

---
+++ RemoveNullValues.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Removing responses with null values
+
+
+# In[ ]:
+
+
+# Accounting For nulls Values On PSS questions 
+df1 = data[['ID','PSSQ1','PSSQ2','PSSQ3','PSSQ4','PSSQ5','PSSQ6','PSSQ7','PSSQ8','PSSQ9','PSSQ10']]
+df1 = df1[df1.isna().any(axis=1)]
+print(df1)
+
+
+# In[ ]:
+
+
+delete_indexes_null_pss = [3, 18, 80, 96, 109, 212, 359]
+
+for ind in delete_indexes_null_pss:
+    data.drop([ind], inplace=True)
+
+
+# In[ ]:
+
+
+# Using the entire dateframe, remove responses that have more than 4 null values (accounting for
+# optional answer to 2 logic open ended question and accidental question skips)
+
+ques = df.columns
+IDNull = []
+
+for i in range(len(df1)):
+    df2 = df.iloc[i]
+    numNaN = 0
+    for x in range(len(ques)):
+        Q = ques[x]
+        if ((df2[Q] is not int) | (df2[Q] is not float)):
+            numNaN += 1
+        if (numNaN == 4):
+            print(df2.ID)
+            IDNull.append(df2.ID)
+            
+
+delete_indexes_too_many_null = [77, 124, 173, 204, 233, 235, 274, 293, 330, 345, 357]
+
+for ind in delete_indexes_too_many_null:
+    data.drop([ind], inplace=True)
+
